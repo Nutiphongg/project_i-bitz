1226a22a8b34cc508060a213ac657860a21c779f
Notto <nottojung2547@gmail.com>
2026-04-01 16:06:26 +0700

    use tanStack and Shadcn ui with mock data

---
+++ frontend/src/data/mock_user.js
@@ -0,0 +1,29 @@
+export const mockMembers = [
+  {
+    member_id: 1,
+    membership_level_id: 101,
+    username: "Chaiwat_P",
+    email: "chaiwat@nu.ac.th",
+    password: "hashed_password_123",
+    status: "active",
+    role: "Admin",
+  },
+  {
+    member_id: 2,
+    membership_level_id: 102,
+    username: "Student_01",
+    email: "somchai@example.com",
+    password: "hashed_password_456",
+    status: "active",
+    role: "user",
+  },
+  {
+    member_id: 3,
+    membership_level_id: 101,
+    username: "Guest_User",
+    email: "guest@test.com",
+    password: "hashed_password_789",
+    status: "suspended",
+    role: "user",
+  },
+];
